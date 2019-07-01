973c3aa9e4dec1f86b9cfe4674a9b38d448d4f1a
Juan David López Londoño <almondbro@AlmondBros-MacBook-Pro.local>
2019-06-30 18:01:04 -0700

    Write HTML for the About Us page.

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -2,9 +2,52 @@ import React from 'react';
 
 const AboutUs = (props) => {
     return (
-        <section>
-            <p>About Us</p>
-        </section>
+        <div className="container">
+            <section className="row">
+                <h2 className="aboutUs-header">Harza Tapes &hypen; About Us</h2>
+                <p>Harza Tapes is the result of a vision of one of 
+                    South America’s most important tape manufacturers, 
+                    Cintandina S.A., a Colombian company with over 50 
+                    years of experience in the pressure sensitive tape 
+                    industry. Recognized as leaders in the Colombian 
+                    industrial market, Cintandina continues its 
+                    international expansion. With the establishment of 
+                    Harza Tapes in California, USA, the company brings 
+                    excellence and value in printed tapes to North 
+                    America.</p>
+                <p>Cintandina is certified under ISO 9001 and its products 
+                    qualify under a number of international standards 
+                    including those of the USA. It employs state-of-the art 
+                    processing equipment unexcelled by any tape manufacturer 
+                    in the world. Technological innovation supported by a 
+                    strong R & D group including technical service has 
+                    enabled the company to be a leader in meeting the markets’ 
+                    evolving needs and expectations and anticipating future 
+                    requirements.
+                </p>
+            </section>
+            <section className="row">
+                <div className="aboutUs-subSection col-sm-6">
+                    <h3>Mission</h3>
+                    <p>
+                        To work as partners with our clients by offering 
+                        pressure sensitive adhesive tape products and 
+                        related services which provide real value.
+                    </p>
+                </div>
+                <div className="aboutUs-subSection col-sm-6">
+                    <h3>Vision</h3>
+                    <p>
+                        To strive for the well being and quality of 
+                        life for our people; to increase the value we 
+                        add to our customers’ businesses; to be good 
+                        stewards of the environment by using the latest 
+                        technologies and materials to minimize environmental 
+                        stress.
+                    </p>
+                </div>
+            </section>
+        </div>   
     );
 };
 
