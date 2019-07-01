7332599ce73a9dff041af77d541c0b914d086143
Juan David López Londoño <almondbro@AlmondBros-MacBook-Pro.local>
2019-06-30 19:24:15 -0700

    Begin HTML for the Location page -- code first location box

--- src/Location.js
+++ src/Location.js
@@ -2,7 +2,32 @@ import React from 'react';
 
 const Location = (props) => {
     return (
-        <p>Location</p>
+        <section className="container">
+            <h3>
+                Our Offices
+            </h3>
+            <div className="row">
+                <div className="col-sm-6">
+                    <h4>United States of America -- Harza Tapes</h4>
+                    <p>8237 Allport Ave</p>
+                    <p>Santa Fe Springs, CA 90670</p>
+                    <a href="tel:562-696-0282">Tel: (562) 696-0282</a>
+                    <a href="mailto:sales@harzatapes.com">sales@harzatapes.com</a>
+                    <a href="https://www.harzatapes.com">www.harzatapes.com</a>
+                </div>
+                <div className="col-sm-6">
+                    
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-6">
+
+                </div>
+                <div className="col-sm-6">
+                    
+                </div>
+            </div>
+        </section>
     );
 };
 
