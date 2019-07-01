bb6718c9c8536551de77d474c4218b681f6384f9
Juan David López Londoño <almondbro@AlmondBros-MacBook-Pro.local>
2019-06-30 20:05:06 -0700

    Finish HTML for the location boxes.

--- src/Location.js
+++ src/Location.js
@@ -7,24 +7,43 @@ const Location = (props) => {
                 Our Offices
             </h3>
             <div className="row">
-                <div className="col-sm-6">
+                <div className="locationBox col-sm-6">
                     <h4>United States of America -- Harza Tapes</h4>
                     <p>8237 Allport Ave</p>
                     <p>Santa Fe Springs, CA 90670</p>
-                    <a href="tel:562-696-0282">Tel: (562) 696-0282</a>
-                    <a href="mailto:sales@harzatapes.com">sales@harzatapes.com</a>
-                    <a href="https://www.harzatapes.com">www.harzatapes.com</a>
+                    <p><a href="tel:562-696-0282">Tel: (562) 696-0282</a></p>
+                    <p><a href="mailto:sales@harzatapes.com">sales@harzatapes.com</a></p>
+                    <p><a href="https://www.harzatapes.com">www.harzatapes.com</a></p>
                 </div>
-                <div className="col-sm-6">
-                    
+                <div className="locationBox col-sm-6">
+                    <h4> Colombia -- Cintandina</h4>
+                    <p>Cra. 2a. No. 37-50</p>
+                    <p>Cali, Colombia</p>
+                    <p><a href="tel:572-443-0801">Tel: (572) 443-0801</a></p>
+                    <p><a href="tel:572-443-0801-0104">Tel: (572) 443-0801 ext. 0104</a></p>
+                    <p><a href="mailto:export@cintandina.com">export@cintandina.com</a></p>
+                    <p><a href="https://www.cintandina.com">www.cintandina.com</a></p>
                 </div>
             </div>
             <div className="row">
-                <div className="col-sm-6">
-
+                <div className="locationBox col-sm-6">
+                    <h4>Costa Rica -- Cintandina</h4>
+                    <p>La Uruca Frente a Mercedes Benz</p>
+                    <p>Ofibodegas JW #29</p>
+                    <p>San Jose, Costa Rica</p>
+                    <p><a href="tel:506-2293-2358">Tel: (506) 2293-2358</a></p>
+                    <p><a href="mailto:costarica@cintandina.com">costarica@cintandina.com</a></p>
+                    <p><a href="https://www.cintandina.com">www.cintandina.com</a></p>
                 </div>
-                <div className="col-sm-6">
-                    
+                <div className="locationBox col-sm-6">
+                    <h4>Ecuador -- Cintandina</h4>
+                    <p>Galo Plaza Lasso</p>
+                    <p>N62-65 y Nazareth</p>
+                    <p>San Jose, Costa Rica</p>
+                    <p><a href="tel:593-2-3464-005">Tel: (593-2) 3464-005</a></p>
+                    <p><a href="tel:593-2-3464-311">Tel: (593-2) 3464-311</a></p>
+                    <p><a href="mailto:costarica@cintandina.com">costarica@cintandina.com</a></p>
+                    <p><a href="https://www.cintandina.com">www.cintandina.com</a></p>
                 </div>
             </div>
         </section>
